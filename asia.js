071405115f7ff4e74e7213491c33d2b624a04d1e
Poovannan M <42384112+Poovannan98@users.noreply.github.com>
2022-08-24 16:12:19 +0530

    Update asia.js

--- asia.js
+++ asia.js
@@ -1,1 +1,15 @@
+const countries = new XMLHttpRequest();
 
+countries.open("GET","https://restcountries.com/v3.1/all");
+countries.send();
+
+countries.responseType = "json";
+
+countries.onload = function AsianCountries(){
+    const asiaCountries = countries.response;
+    const Names = asiaCountries
+         .filter((n) => n.region === "Asia")
+         .map((n) => n.name.common);
+    
+    console.log(Names);
+}
